444f3179f42ec8634c03c0c23907d903ca0af327
Stupidgirl <2528532609@qq.com>
2017-07-01 20:32:39 +0800

    app.js

--- app/src/components/app.js
+++ app/src/components/app.js
@@ -4,15 +4,21 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
+import { Card, WingBlank, WhiteSpace } from 'antd-mobile';
 import logo from '../images/logo.svg'
-
+import Welcome from '../welcome/Welcome'
+import ToDo from '../components/todo/toDo'
+import MyNeedDealt from '../components/MyNeedDealt/MyNeedDealt'
 import './app.less'
+
+
 class AppComponent extends Component {
   render () {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
@@ -25,5 +31,6 @@ class AppComponent extends Component {
     )
   }
 }
+
 
 export default AppComponent
